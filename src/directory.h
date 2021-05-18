bbc1f39716ed13d1d9f48c6de7fa12b9e3a11ef9
mirahumaira <80517206+mirahumaira@users.noreply.github.com>
2021-05-18 17:27:58 -0400

    Update directory.h

--- src/directory.h
+++ src/directory.h
@@ -18,20 +18,32 @@
 class Directory{
 
 private:
+   
+    // File directory. "Your directory/Files/Sorted_Farmers_Market.txt"
     const QString sortedFileDirectory = "C:/Users/afrah/OneDrive/Documents/Food4All/Files/Sorted_Farmers_Market.txt";
-
+    
+   // File directory. "Your directory\\Files\\Farmers_Market_Bronx.txt"
     const QString bronxMasterFileDirectory = "C:\\Users\\afrah\\OneDrive\\Documents\\Food4All\\Files\\Farmers_Market_Bronx.txt";
+   // File directory. "Your directory\\Files\\Farmers_Market_Brooklyn.txt"
     const QString brooklynMasterFileDirectory = "C:\\Users\\afrah\\OneDrive\\Documents\\Food4All\\Files\\Farmers_Market_Brooklyn.txt";
+   // File directory. "Your directory\\Files\\Farmers_Market_Manhattan.txt"
     const QString manhattanMasterFileDirectory = "C:\\Users\\afrah\\OneDrive\\Documents\\Food4All\\Files\\Farmers_Market_Manhattan.txt";
+   // File directory. "Your directory\\Files\\Farmers_Market_Queens.txt"
     const QString queensMasterFileDirectory = "C:\\Users\\afrah\\OneDrive\\Documents\\Food4All\\Files\\Farmers_Market_Queens.txt";
+   // File directory. "Your directory\\Files\\Farmers_Market_StatenIsland.txt"
     const QString statenIslandMasterFileDirectory = "C:\\Users\\afrah\\OneDrive\\Documents\\Food4All\\Files\\Farmers_Market_StatenIsland.txt";
 
+    // Image directory. "Your directory/media/BackgroudBorough.jpg"
     const QString boroughBackgroundImageDirectory = "C:/Users/afrah/OneDrive/Documents/Food4All/media/BackgroudBorough.jpg";
+   // Image directory. "Your directory/media/Backgroud-img.jpg"
     const QString backgroundImageDark = "C:/Users/afrah/OneDrive/Documents/Food4All/media/Backgroud-img.jpg";
-
+   // Image directory. "Your directory/media/prediabetes1.jpg"
     const QString diabetesBackgroundDirectory1 = "C:/Users/afrah/OneDrive/Documents/Food4All/media/prediabetes1.jpg";
+   // Image directory. "Your directory/media/diabetes2.jpg"
     const QString diabetesBackgroundDirectory2 = "C:/Users/afrah/OneDrive/Documents/Food4All/media/diabetes2.jpg";
+   // Image directory. "Your directory/media/Backgroud-img - Obesity.jpg"
     const QString obesityBackgroundDirectory = "C:/Users/afrah/OneDrive/Documents/Food4All/media/Backgroud-img - Obesity.jpg";
+   // Image directory. "Your directory/media/Hypertension1.jpg"
     const QString hypertensionBackgroundDirectory = "C:/Users/afrah/OneDrive/Documents/Food4All/media/Hypertension1.jpg";
 
 
